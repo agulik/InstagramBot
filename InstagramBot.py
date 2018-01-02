78522ee8c5bbad131a0083b49cfbc23d4a8717e8
Andrew Gulik <andrewgulik@gmail.com>
2018-01-02 18:33:03 -0500

    finished login function

--- InstagramBot.py
+++ InstagramBot.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 
 driver = webdriver.Chrome('./assets/chromedriver')
 insta_username = ''
@@ -14,19 +15,32 @@ class InstagramBot():
         time.sleep(5) # Let the user actually see something!
 
         # if on sign up page, switch to login
-
+        # if there is a mobile field, we know we are on the sign up page
         mobile_field = driver.find_element_by_xpath(
             '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[2]/div/input[@placeholder="Mobile Number or Email"]'
         )
+        switch_to_login_button = driver.find_element_by_xpath(
+            '//*[@id="react-root"]/section/main/article/div[2]/div[2]/p/a'
+        )
         if mobile_field is not None:
-            print("you are on the sign-up page")
-        # login_box.send_keys(insta_username)
-        # search_box.submit()
-        # time.sleep(5) # Let the user actually see something!
-        # first_result = driver.find_element_by_xpath('/html/body/div[4]/div[3]/div/div/div[1]/div/header/a')
-        # first_result.click()
-        # time.sleep(5) # Let the user actually see something!
-        # driver.quit()
+            print("switching from sign-up page to login page")
+            ActionChains(driver).move_to_element(switch_to_login_button).click().perform()
+        
+        # if already on sign-up page, login
+        username_field = driver.find_element_by_xpath(
+            '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[1]/div/input[@name="username"]'
+        )
+        password_field = driver.find_element_by_xpath(
+            '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[2]/div/input'
+        )
+        login_button = driver.find_element_by_xpath(
+            '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/span/button'
+        )
+        if username_field is not None:
+            print("logging user credentials...")
+            username_field.send_keys(insta_username)
+            password_field.send_keys(insta_password)
+            login_button.click()
 
 session = InstagramBot()
 session.login_user()
